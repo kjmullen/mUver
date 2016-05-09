dbde9af47f9a1983dd05957ad00b5d4b79ad7002
Kevin Mullen <kevinkozzik@gmail.com>
2016-05-09 11:28:44 -0700

    updated pagination to 15 in settings

--- mUver/settings.py
+++ mUver/settings.py
@@ -222,7 +222,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 5,
+    'PAGE_SIZE': 15,
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
