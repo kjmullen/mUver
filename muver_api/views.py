6db052693a26fa5d3076329f0c81e227b6adaa97
Kevin Mullen <kevinkozzik@gmail.com>
2016-05-11 15:15:54 -0700

    in progress jobs fixed to not show when user/mover confirmed

--- muver_api/views.py
+++ muver_api/views.py
@@ -97,11 +97,11 @@ class JobsByUser(generics.ListAPIView):
     def get_queryset(self):
         user = self.request.user
         if not user.profile.mover:
-            return Job.objects.filter(user=user).filter(complete=False).order_by("-modified_at")
+            return Job.objects.filter(user=user).filter(confirmation_user=True).order_by("-modified_at")
         else:
             profile = user.profile
             return Job.objects.filter(mover_profile=profile.id)\
-                .filter(complete=False).order_by("-modified_at")
+                .filter(confirmation_user=False).order_by("-modified_at")
 
 
 class CompletedJobsByUser(generics.ListAPIView):
